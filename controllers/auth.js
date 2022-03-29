4a833aaf79bc47b80a6c907e700587b553a6e83f
Viet Anh Ngo <ngovietanh503@gmail.com>
2022-03-29 16:16:51 +0700

    auth

--- controllers/auth.js
+++ controllers/auth.js
@@ -27,5 +27,31 @@ export const register = async (req, res) => {
     }
 }
 export const login = async (req, res) => {
-    
+    const { email, password} = req.body;
+    try {
+        const user = await User.findOne({email}).exec();
+        if(!user){
+            res.status(400).json({
+                message: "Email không tồn tại"
+            })
+        }
+        if(!user.authenticate(password)){
+            res.status(400).json({
+                message: "Mật khẩu không đúng"
+            })
+        }
+        const token = jwt.sign({_id: user._id}, "123456", { expiresIn: '1h'})
+        res.json({
+            token,
+            user: {
+                _id: user._id,
+                email: user.email,
+                name: user.name
+            }
+        })
+    } catch (error) {
+        res.status(400).json({
+            message: "Đăng nhập thất bại"
+        })
+    }
 }
