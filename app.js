dfcea1f47b24d7ccf7ce04b5ab2ef83cdf0246a0
Viet Anh Ngo <ngovietanh503@gmail.com>
2022-04-06 12:40:43 +0700

    feat: nhiều thừ :))

--- app.js
+++ app.js
@@ -6,12 +6,15 @@ import authRoute from './routes/auth';
 import cors from 'cors'
 import mongoose from 'mongoose';
 import categoryroute from './routes/category';
+var bodyParser = require('body-parser');
 
 const app = express();
 //middleware
-// app.use(cors())
-// app.use(morgan('tiny'))
-// app.use(express.json())
+app.use(cors())
+app.use(express.json())
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 app.use(express.json())
 app.use(homeroute);
